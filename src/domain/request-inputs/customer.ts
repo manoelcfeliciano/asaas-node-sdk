54f313a80bde430ad5c577045ba7b6f42355d261
Manoel Feliciano <manoel.feliciano@liberedu.com>
2023-01-09 16:47:16 -0300

    feat: add Customer request-input

---
+++ src/domain/request-inputs/customer.ts
@@ -0,0 +1,39 @@
+export namespace CustomerRequestInput {
+	export namespace Create {
+		export type Body = {
+			name: string;
+			cpfCnpj: string;
+			email?: string;
+			phone?: string;
+			mobilePhone?: string;
+			address?: string;
+			addressNumber?: string;
+			complement?: string;
+			province?: string;
+			postalCode?: string;
+			externalReference?: string;
+			notificationDisabled?: boolean;
+			additionalEmails?: string;
+			municipalInscription?: string;
+			stateInscription?: string;
+			observations?: string;
+			groupName?: string;
+		};
+	}
+
+	export namespace Update {
+		export type Body = Partial<CustomerRequestInput.Create.Body>;
+	}
+
+	export namespace GetList {
+		export type Params = {
+			name: string;
+			email: string;
+			cpfCnpj: string;
+			groupName: string;
+			externalReference: string;
+			offset: number;
+			limit: number;
+		};
+	}
+}
