a11b02610c9fe065baa1f4cabdc7490b11b5d0c5
Manoel Feliciano <manoel.feliciano@liberedu.com>
2023-01-16 13:14:18 -0300

    feat: add payments method to sdk root

--- src/main/asaas-sdk.ts
+++ src/main/asaas-sdk.ts
@@ -2,9 +2,11 @@ import { BaseResourceConstructor } from '~/data/protocols/base-resource';
 import { CustomersResource } from '~/data/resources/customers-resource';
 import { makeHttpRequestClient } from './factories/http-request-client';
 import { AsaasSdkConfig } from './protocols/asaas-sdk-config';
+import { PaymentsResource } from '../data/resources/payments-resource';
 
 export class AsaasSdk {
 	private readonly _customers: CustomersResource;
+	private readonly _payments: PaymentsResource;
 
 	constructor(config: AsaasSdkConfig) {
 		const httpClient = makeHttpRequestClient(config);
@@ -13,9 +15,14 @@ export class AsaasSdk {
 		};
 
 		this._customers = new CustomersResource(resourceConstructor);
+		this._payments = new PaymentsResource(resourceConstructor);
 	}
 
 	get customers() {
 		return this._customers;
+	}
+
+	get payments() {
+		return this._payments;
 	}
 }
