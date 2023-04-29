2b03a1b15655e2f4fb8b34d61921ed6e4469aaed
Daniel Amaral <danielamaral.si@gmail.com>
2023-04-29 14:40:33 -0300

    feat: add paymentLinks resource

--- src/main/asaas-sdk.ts
+++ src/main/asaas-sdk.ts
@@ -1,14 +1,15 @@
-
 import { makeHttpRequestClient } from './factories/http-request-client';
 import { AsaasSdkConfig } from './protocols/asaas-sdk-config';
 import { PaymentsResource } from '../data/resources/payments-resource';
 import { CustomersResource } from '../data/resources/customers-resource';
 import { InstallmentsResource } from '../data/resources/installments-resource';
 import { BaseResourceConstructor } from '../data/protocols/base-resource';
+import { PaymentLinksResource } from '../data/resources/payment-links-resource';
 
 export class AsaasSdk {
 	private readonly _customers: CustomersResource;
 	private readonly _payments: PaymentsResource;
+	private readonly _paymentLinks: PaymentLinksResource;
 	private readonly _installments: InstallmentsResource;
 
 	constructor(config: AsaasSdkConfig) {
@@ -19,6 +20,7 @@ export class AsaasSdk {
 
 		this._customers = new CustomersResource(resourceConstructor);
 		this._payments = new PaymentsResource(resourceConstructor);
+		this._paymentLinks = new PaymentLinksResource(resourceConstructor);
 		this._installments = new InstallmentsResource(resourceConstructor);
 	}
 
@@ -28,6 +30,10 @@ export class AsaasSdk {
 
 	get payments() {
 		return this._payments;
+	}
+
+	get paymentLinks() {
+		return this._paymentLinks;
 	}
 
 	get installments() {
